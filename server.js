6586df35c5f9ee2119d1108ddaa6528db955e2bb
Pat Yolian <pyolian@gmail.com>
2016-09-18 18:06:21 -0700

    set up cheerio

--- server.js
+++ server.js
@@ -7,6 +7,28 @@ const port = process.env.PORT || 1300;
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'bower_components')));
+app.use(express.static(path.join(__dirname, 'client')));
+
+app.get('/scrape', function(req, res) {
+    var url = "http://games.espn.com/ffl/activestats?leagueId=1313111&teamId=3&filter=0";
+
+    request(url, function(err, response, html) {
+        // console.log(html);
+
+        if(!err) {
+            console.log("yo! no errors");
+            $ = cheerio.load(html);
+            // $('#playerTableContainerDiv').filter(function() {
+            //     var data = $(this);
+            //     console.log(data);
+            // });
+            $('table');
+        }
+
+        // console.log(cheerio);
+    });
+});
+
 app.listen(port, function() {
     console.log("Listening on " + port);
-})
+});
